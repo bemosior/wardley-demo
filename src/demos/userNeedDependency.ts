1470f3877a6d8027f9e9294cafd005378d5c8347
Ben Mosior <bemosior@gmail.com>
2026-06-22 22:40:51 -0400

    layout vertical

--- src/demos/userNeedDependency.ts
+++ src/demos/userNeedDependency.ts
@@ -4,10 +4,10 @@ export const userNeedDependencyDemo: DemoConfig = {
   viewBox: { width: 400, height: 300 },
   snapThreshold: 30,
   nodes: [
-    { id: "user", label: "User", x: 200, y: 40, draggable: false },
-    { id: "need", label: "Need", x: 200, y: 160, draggable: true, start: { x: 350, y: 160 } },
-    { id: "dependency-1", label: "Dependency", x: 130, y: 260, draggable: false },
-    { id: "dependency-2", label: "Dependency", x: 270, y: 260, draggable: false },
+    { id: "user", label: "User", x: 200, y: 26, draggable: false },
+    { id: "need", label: "Need", x: 200, y: 107, draggable: true, start: { x: 35, y: 107 } },
+    { id: "dependency-1", label: "Dependency", x: 130, y: 189, draggable: false },
+    { id: "dependency-2", label: "Dependency", x: 270, y: 189, draggable: false },
   ],
   connections: [
     { from: "user", to: "need" },
